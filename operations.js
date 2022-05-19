11719d20e86b0cc54613c778e5d6901bb18c5d99
Ande Caleb <andaeiii@aol.com>
2022-05-19 14:00:09 +0100

    made changes to the operations

--- operations.js
+++ operations.js
@@ -1,6 +1,16 @@
 const { parseFloat } = require("core-js/core/number");
 
-function add(x,y) {
+function getVals(x, y) {
+  return [parseInt(x), parseInt(y)];
+}
 
-    return parseInt(x) + parseInt(y)
+function add(x, y) {
+  let [a, b] = getVals(x, y);
+  return a + b;
+}
+
+
+//this is the next function ... 
+function substract(x, y) {
+  return parseInt(x) - parseInt(y);
 }
